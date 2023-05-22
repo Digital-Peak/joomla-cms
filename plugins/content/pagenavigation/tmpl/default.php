2ff17008b9db321ce33a50fa0ceca39f3cdfb4a3
Christian Heel <66922325+heelc29@users.noreply.github.com>
2023-05-22 18:47:09 +0200

    Revert pagenavigation

--- plugins/content/pagenavigation/tmpl/default.php
+++ plugins/content/pagenavigation/tmpl/default.php
@@ -10,11 +10,12 @@
 
 defined('_JEXEC') or die;
 
+use Joomla\CMS\Factory;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Router\Route;
 
-$lang = $this->getApplication()->getLanguage();
-?>
+$lang = Factory::getLanguage(); ?>
+
 <nav class="pagenavigation">
     <span class="pagination ms-0">
     <?php if ($row->prev) :
